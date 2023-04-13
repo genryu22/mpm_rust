5af0cb1db09aa685fc3bdb1faaf0e3e54f37401f
Genryu Inami <genryu7@gmail.com>
2023-04-13 14:34:09 +0900

    add bevy sample

--- mpm_rust/viewer/src/window_bevy.rs
+++ mpm_rust/viewer/src/window_bevy.rs
@@ -3,10 +3,16 @@ use std::{
     sync::mpsc,
 };
 
-use bevy::{prelude::*, DefaultPlugins};
+use bevy::{prelude::*, render::camera::ScalingMode, DefaultPlugins};
 use bevy_points::{material::PointsShaderSettings, prelude::*};
 
 use crate::Snapshot;
+
+#[derive(Component, Default)]
+struct Spiral;
+
+#[derive(Component)]
+struct Count(usize);
 
 pub fn run(snapshot_receiver: mpsc::Receiver<Snapshot>) {
     App::new()
@@ -15,6 +21,7 @@ pub fn run(snapshot_receiver: mpsc::Receiver<Snapshot>) {
         .insert_resource(ClearColor(Color::rgb(0.01, 0.02, 0.08)))
         .insert_non_send_resource(snapshot_receiver)
         .add_startup_system(setup)
+        .add_system(test_animate)
         .add_system(update)
         .run();
 }
@@ -24,44 +31,117 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<PointsMaterial>>,
 ) {
-    let ORIGIN: Vec3 = Vec3::new(0.0, 0.0, -5.0);
-    let mut pt = PointsMesh::from(Mesh::from(shape::UVSphere {
-        radius: 1.0,
-        sectors: 36,
-        stacks: 18,
-    }));
-    let n = pt.vertices.len() as f32;
-    pt.colors = Some(
-        pt.vertices
+    commands.spawn(Count(0));
+
+    commands.spawn((Spiral::default(), create_points_mesh(meshes, materials)));
+
+    commands.spawn(Camera3dBundle {
+        projection: OrthographicProjection {
+            near: 0.1,
+            far: 100.,
+            scale: 1.0,
+            scaling_mode: ScalingMode::WindowSize(200.),
+            ..Default::default()
+        }
+        .into(),
+        transform: Transform::from_translation(Vec3::new(0.0, 0.0, -5.0))
+            .looking_at(Vec3::ZERO, Vec3::Y),
+        ..Default::default()
+    });
+}
+
+fn test_animate(
+    mut meshes: ResMut<Assets<Mesh>>,
+    mesh_query: Query<&Handle<Mesh>, With<Spiral>>,
+    mut count: Query<&mut Count>,
+) {
+    if mesh_query.is_empty() {
+        return;
+    }
+    let mesh_handle = mesh_query.single();
+    if !meshes.contains(mesh_handle) {
+        return;
+    }
+    let mesh = meshes.get_mut(mesh_handle).unwrap();
+    let vertices = mesh.attribute_mut(Mesh::ATTRIBUTE_POSITION);
+    if vertices.is_none() {
+        return;
+    }
+    let mut frame_count = count.get_single_mut().unwrap();
+    let vertices = vertices.unwrap();
+    let v_sum = vertices.len() / 4;
+    let h = 3.0;
+    update_mesh(
+        mesh,
+        PointsMesh::from_iter((0..v_sum).map(|i| {
+            let t01 =
+                ((i as f32 + frame_count.0 as f32 / 10.) % v_sum as f32) / ((v_sum - 1) as f32);
+            let r = t01 * TAU * 4.0;
+            Vec3::new(r.cos(), (t01 - 0.5) * h, r.sin())
+        })),
+    );
+    frame_count.0 += 1;
+}
+
+fn update_mesh(mesh: &mut Mesh, points_mesh: PointsMesh) {
+    let vertices: Vec<[f32; 3]> = points_mesh
+        .vertices
+        .iter()
+        .flat_map(|p| {
+            let arr = p.to_array();
+            [arr, arr, arr, arr]
+        })
+        .collect();
+    let uv_set = [[0., 0.], [1., 0.], [1., 1.], [0., 1.]];
+    let uvs: Vec<[f32; 2]> = points_mesh.vertices.iter().flat_map(|_| uv_set).collect();
+    let indices = bevy::render::mesh::Indices::U32(
+        points_mesh
+            .vertices
             .iter()
             .enumerate()
-            .map(|(i, _)| {
-                let t = (i as f32) / (n - 1.) * 360.;
-                Color::hsl(t, 1., 0.5)
+            .flat_map(|(i, _)| {
+                let idx = (i * 4) as u32;
+                [idx, idx + 1, idx + 3, idx + 2, idx + 3, idx + 1]
             })
             .collect(),
     );
+    mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, vertices);
+    mesh.insert_attribute(Mesh::ATTRIBUTE_UV_0, uvs);
+    if let Some(color) = points_mesh.colors {
+        mesh.insert_attribute(
+            Mesh::ATTRIBUTE_COLOR,
+            color
+                .iter()
+                .flat_map(|c| {
+                    let arr = c.as_rgba_f32();
+                    [arr, arr, arr, arr]
+                })
+                .collect::<Vec<[f32; 4]>>(),
+        );
+    }
+    mesh.set_indices(Some(indices));
+}
 
-    commands.spawn(MaterialMeshBundle {
-        mesh: meshes.add(pt.into()),
-        material: materials.add(PointsMaterial {
-            settings: PointsShaderSettings {
-                point_size: 0.1,
-                opacity: 0.5,
-                ..Default::default()
-            },
-            perspective: true,
-            alpha_mode: AlphaMode::Blend,
-            ..Default::default()
-        }),
-        transform: Transform::from_translation(Vec3::NEG_X * 1.25)
-            .with_rotation(Quat::from_axis_angle(Vec3::ONE.normalize(), FRAC_PI_2)),
-        ..Default::default()
-    });
+fn update(world: &mut World) {
+    let receiver = world.get_non_send_resource::<mpsc::Receiver<Snapshot>>();
+    if receiver.is_none() {
+        return;
+    }
+    let receiver = receiver.unwrap();
 
+    if let Ok(snapshot) = receiver.try_recv() {
+        println!("{}", snapshot.steps);
+    }
+}
+
+fn create_points_mesh(
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<PointsMaterial>>,
+) -> MaterialMeshBundle<PointsMaterial> {
     let n = 320;
     let h = 3.0;
-    commands.spawn(MaterialMeshBundle {
+
+    MaterialMeshBundle {
         mesh: meshes.add(
             PointsMesh::from_iter((0..n).map(|i| {
                 let t01 = (i as f32) / ((n - 1) as f32);
@@ -80,31 +160,6 @@ fn setup(
             circle: true,
             ..Default::default()
         }),
-        transform: Transform::from_translation(Vec3::X * 1.25),
         ..Default::default()
-    });
-
-    commands.spawn(Camera3dBundle {
-        projection: PerspectiveProjection {
-            fov: 45.0,
-            aspect_ratio: 1.,
-            near: 0.1,
-            far: 100.,
-        }
-        .into(),
-        transform: Transform::from_translation(ORIGIN).looking_at(Vec3::ZERO, Vec3::Y),
-        ..Default::default()
-    });
-}
-
-fn update(world: &mut World) {
-    let receiver = world.get_non_send_resource::<mpsc::Receiver<Snapshot>>();
-    if receiver.is_none() {
-        return;
-    }
-    let receiver = receiver.unwrap();
-
-    if let Ok(snapshot) = receiver.try_recv() {
-        println!("{}", snapshot.steps);
     }
 }
